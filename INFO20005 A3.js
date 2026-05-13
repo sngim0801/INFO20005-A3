13d67182e0cfd4bfd07e62832826a67c6ae9f97f
Sher Lyn <sngim@student.unimelb.edu.au>
2026-05-14 00:22:53 +1000

    Commit 13: Linked all pages and done 'add to cart' JavaScript function

---
+++ INFO20005 A3.js
@@ -0,0 +1,12 @@
+function addToCart() {
+    const addButton = document.getElementById("addButton");
+    addButton.textContent = "Added to cart";
+    addButton.style.backgroundColor = "#B3E0DC";
+
+    const bagBadge = document.getElementById("bagBadge");
+    document.getElementById("bagBadge").style.display = "flex";
+}
+
+function submitForm() {
+    
+}
