d02ce5f48af865548a91c35ff2faf66e39d13cb6
Sher Lyn <sngim@student.unimelb.edu.au>
2026-05-17 23:23:57 +1000

    Commit 14: Implemented JavaScript for select all checkboxes logic

--- INFO20005 A3.js
+++ INFO20005 A3.js
@@ -7,6 +7,23 @@ function addToCart() {
     document.getElementById("bagBadge").style.display = "flex";
 }
 
-function submitForm() {
-    
+function checkboxes() {
+  document.getElementById("selectAll").addEventListener("change", function() {
+    const productCheckboxes = document.querySelectorAll(".productCheckbox");
+    productCheckboxes.forEach(function(checkbox) {
+      checkbox.checked = this.checked;
+    }, this);
+  });
+
+  document.querySelectorAll(".productCheckbox").forEach(function(checkbox) {
+    checkbox.addEventListener("change", function() {
+      const all = document.querySelectorAll(".productCheckbox");
+      const allChecked = [...all].every(cb => cb.checked);
+      document.getElementById("selectAll").checked = allChecked;
+    });
+  });
+}
+
+window.onload = function() {
+  checkboxes();
 }
